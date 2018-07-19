a82473ad6205767c40ca0a101615b057a3b6fe73
Daniel Franssen <daan-franssen@live.nl>
2018-07-19 10:22:36 +0000

    first commit 2X2

---
+++ test_boggle.py
@@ -0,0 +1,10 @@
+import unittest
+from boggle import *
+
+class TestBoggle(unittest.TestCase):
+    
+    def test_is_this_thing_on(self):
+        self.assertTrue(check())
+        
+        
+    
